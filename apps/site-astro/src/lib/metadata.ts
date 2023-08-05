3e948225856a8a9699f1a04b3838007cad0f762b
David Witt <midknight55@gmail.com>
2023-08-04 18:37:53 -0700

    Fix URL for sharing image

--- apps/site-astro/src/lib/metadata.ts
+++ apps/site-astro/src/lib/metadata.ts
@@ -4,7 +4,7 @@ export const siteDescription =
   "Manifest Destiny of the (Second) Best Damn Football Conference in the Land";
 export const hostname =
   process.env.NODE_ENV === "production"
-    ? "https://bigtenafterdark.com/"
+    ? "https://big10afterdark.com/"
     : "https://localhost:3000/";
 
 export function titleTemplate(title?: string, tagline: string = siteTagline) {
